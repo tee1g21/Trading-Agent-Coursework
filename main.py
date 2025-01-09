c77b63e19e6d47942fb7f7bf728ceff213efaeaf
D4nDude <hobbs.daniel.007@gmail.com>
2025-01-09 04:04:47 +0000

    docs: explaining my main.py test file

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from agents.Nathaniel.Dumbass2 import Dumbass2
 from agents.testing.test import TestingEnvironment
 from agents.Tom.SuperCoolCompany import SuperCoolCompany
 
+# Create our test environment
 test_environment = TestingEnvironment()
 
 # test_environment.setup_environment(30, 5, 5)
 
+# Add our companies we want to test
 test_environment.setup_companies([Dumbass2, SuperCoolCompany])
+
+# setup our fleet combinations from 0 up to provided numbers
 test_environment.setup_random_fleets(1, 1, 1)
 
+# run the tests with a sample of 5 on 6 threads (change to None for sequential runs)
 test_environment.run_tests(5, 6)
